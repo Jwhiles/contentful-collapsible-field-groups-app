1234d2c017bd4ecd78608a967f18ca0cc2c80c93
John Whiles <john.whiles@contentful.com>
2020-07-28 11:42:33 +0200

    feat(config screen): allow selection of interface

--- src/ConfigScreen.tsx
+++ src/ConfigScreen.tsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import { AppExtensionSDK } from "contentful-ui-extensions-sdk";
-import { TextField, Heading, Form, Workbench } from "@contentful/forma-36-react-components";
+import {
+  Heading,
+  Form,
+  Workbench,
+  Select,
+  Option,
+} from "@contentful/forma-36-react-components";
 import { css } from "emotion";
 
 export interface AppInstallationParameters {
@@ -13,12 +19,18 @@ interface ConfigProps {
 
 interface ConfigState {
   parameters: AppInstallationParameters;
+  contentTypes: any[];
+  selectedContentType: string;
 }
 
 export default class Config extends Component<ConfigProps, ConfigState> {
   constructor(props: ConfigProps) {
     super(props);
-    this.state = { parameters: { defaultValue: "" } };
+    this.state = {
+      contentTypes: [],
+      selectedContentType: "",
+      parameters: { defaultValue: "" },
+    };
 
     // `onConfigure` allows to configure a callback to be
     // invoked when a user attempts to install the app or update
@@ -26,35 +38,91 @@ export default class Config extends Component<ConfigProps, ConfigState> {
     props.sdk.app.onConfigure(() => this.onConfigure());
   }
 
+  async getContentTypesUsingEditor(): Promise<any> {
+    const { space, ids } = this.props.sdk;
+    const editorInterfaces = await space.getEditorInterfaces();
+
+    const appId = ids.app;
+
+    const appIncludedInEditors = (appId: any, editorInterface: any) => {
+      if (editorInterface.editor) {
+        return appId === editorInterface.editor.widgetId;
+      } else if (editorInterface.editors) {
+        return editorInterface.editors.some(
+          ({ widgetId }: any) => widgetId === appId
+        );
+      }
+    };
+
+    const entryIds: any = editorInterfaces.items.reduce(
+      (contentTypes, editorInterface: any) => {
+        if (appIncludedInEditors(appId, editorInterface)) {
+          const contentTypeId = editorInterface.sys?.contentType?.sys?.id;
+          return contentTypes.concat(contentTypeId);
+        }
+        return contentTypes;
+      },
+      []
+    );
+
+    const entries = await Promise.all(
+      entryIds.map((id: string) => space.getContentType(id))
+    );
+
+    return entries;
+  }
+
   async componentDidMount() {
     // Get current parameters of the app.
     // If the app is not installed yet, `parameters` will be `null`.
     const parameters: AppInstallationParameters | null = await this.props.sdk.app.getParameters();
+    const contentTypes = await this.getContentTypesUsingEditor();
 
-    this.setState(parameters ? { parameters } : this.state, () => {
-      // Once preparation has finished, call `setReady` to hide
-      // the loading screen and present the app to a user.
-      this.props.sdk.app.setReady();
-    });
+    this.setState(
+      parameters
+        ? {
+            parameters,
+            contentTypes,
+            selectedContentType: contentTypes[0]?.sys.id,
+          }
+        : this.state,
+      () => {
+        // Once preparation has finished, call `setReady` to hide
+        // the loading screen and present the app to a user.
+        this.props.sdk.app.setReady();
+      }
+    );
   }
 
   render() {
     return (
       <Workbench className={css({ margin: "80px" })}>
         <Form>
-          <Heading>App Config</Heading>
-          <TextField
-            name="config"
-            id="config"
-            labelText="Set a default value to fill in your field editor"
-            value={this.state.parameters.defaultValue}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              this.setState({ parameters: { defaultValue: e.target.value } })
-            }
-          />
+          <Heading>Field Group Set Up Config</Heading>
+          <Select
+            value={this.state.selectedContentType}
+            onChange={(e: any) => {
+              // @ts-ignore
+              this.setState({ selectedContentType: e.target.value });
+            }}
+          >
+            {this.state.contentTypes.map((ct) => (
+              <Option key={ct.sys.id} value={ct.sys.id}>
+                {ct.name}
+              </Option>
+            ))}
+          </Select>
         </Form>
+        <div>
+          here appears the group editors for selected thing{" "}
+          <span>{this.getInterface(this.state.selectedContentType)}</span>
+        </div>
       </Workbench>
     );
+  }
+
+  getInterface(x: string): string {
+    return this.state.contentTypes.find((ct) => ct.sys.id === x)?.name ?? "";
   }
 
   async onConfigure() {
@@ -67,10 +135,13 @@ export default class Config extends Component<ConfigProps, ConfigState> {
       if (editorInterface.editor) {
         return appId === editorInterface.editor.widgetId;
       } else if (editorInterface.editors) {
-        return editorInterface.editors.some(({ widgetId }: any) => widgetId === appId);
+        return editorInterface.editors.some(
+          ({ widgetId }: any) => widgetId === appId
+        );
       }
     };
 
+    console.log("I need to set up the tabs and stuff better");
     const contentTypesUsingApp = editorInterfaces.items.reduce(
       (contentTypes, editorInterface: any) => {
         if (appIncludedInEditors(appId, editorInterface)) {
@@ -82,9 +153,12 @@ export default class Config extends Component<ConfigProps, ConfigState> {
       []
     );
 
-    const EditorInterface = contentTypesUsingApp.reduce((edInt, contentTypeId) => {
-      return { ...edInt, [contentTypeId]: { editor: true } };
-    }, {});
+    const EditorInterface = contentTypesUsingApp.reduce(
+      (edInt, contentTypeId) => {
+        return { ...edInt, [contentTypeId]: { editor: true } };
+      },
+      {}
+    );
 
     return {
       parameters: this.state.parameters,
